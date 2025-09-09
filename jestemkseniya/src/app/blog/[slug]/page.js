bfa50640fda3a151598da91cff06c0cdbbce9d5a
Kamil Barwiński <kamilb.kontakt@gmail.com>
2025-09-09 08:18:02 +0200

    Update page.js

--- jestemkseniya/src/app/blog/[slug]/page.js
+++ jestemkseniya/src/app/blog/[slug]/page.js
@@ -1,6 +1,7 @@
 import { getAllPostSlugs, getPostBySlug } from '@/lib/posts'
 import Prose from '@/components/Prose'
 import { notFound } from 'next/navigation'
+import Link from 'next/link'
 
 export async function generateStaticParams() {
   const slugs = await getAllPostSlugs()
@@ -14,7 +15,7 @@ export default async function PostPage({ params }) {
     return (
       <main className="p-6 min-h-dvh">
         <div className="max-w-5xl mx-auto mb-6">
-          <a href="/blog" className="underline">← Wróć na blog</a>
+          <Link href="/blog" className="underline">← Wróć na blog</Link>
         </div>
         <Prose>
           <h1>{meta.title}</h1>
